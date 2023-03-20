4270f49929bf2a87b8fcccc2c6c8e0d4d11d4a50
Michael E. Ershov <em@newton.finance>
2023-03-20 09:11:16 +0800

    Added colors and fonts

--- src/views/Header/Header.tsx
+++ src/views/Header/Header.tsx
@@ -1,22 +1,28 @@
 import React, { FC, memo } from "react";
 
+import "./Header.scss";
+
 const Header: FC = () => {
   return (
     <header className="page-header">
       <header className="page-header__header">
         <div className="logo">
-          <div className="logo__logo"></div>
-          <div className="logo__label"></div>
+          <img className="logo__logo" />
+          <h2 className="logo__label"></h2>
         </div>
         <div className="contacts">
-          <div className="contacts__phone">+7 (495) 177-65-96</div>
-          <div className="contacts__email">info@helpdevelopment.ru</div>
+          <a className="contacts__phone" href="tel:+74951776596">
+            +7 (495) 177-65-96
+          </a>
+          <a className="contacts__email" href="mailto:info@helpdevelopment.ru">
+            info@helpdevelopment.ru
+          </a>
         </div>
         <button>Заказать звонок</button>
       </header>
       <footer className="page-header__footer">
         <nav>
-          <div>-</div>
+          <svg>-</svg>
           <ul>
             <li>Услуги</li>
             <li>О компании</li>
