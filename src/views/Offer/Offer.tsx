841e0ea1ef2e890a769b2230aef9af86f1c8b1e5
Michael E. Ershov <em@newton.finance>
2023-03-24 10:35:55 +0800

    Added offer

--- src/views/Offer/Offer.tsx
+++ src/views/Offer/Offer.tsx
@@ -1,20 +1,24 @@
 import React, { FC, memo } from "react";
 
+import "./Offer.scss";
+
 const Offer: FC = () => {
   return (
     <section className="offer">
-      <h1>Легализция самостроя</h1>
-      <ul>
-        <li>Легализация через ГЗК</li>
-        <li>Узаконивание невыявленных объектов</li>
-        <li>Узаконивание через суд</li>
-      </ul>
-      <ul>
-        <li>от 150 тыс. рублей</li>
-        <li>от 60 дней</li>
-        <li>гарантия</li>
-      </ul>
-      <button>Бесплатная консультация</button>
+      <div className="container">
+        <h1 className="offer__header">Легализация самостроя</h1>
+        <ul>
+          <li>Легализация через ГЗК</li>
+          <li>Узаконивание невыявленных объектов</li>
+          <li>Узаконивание через суд</li>
+        </ul>
+        <ul>
+          <li>от 150 тыс. рублей</li>
+          <li>от 60 дней</li>
+          <li>гарантия</li>
+        </ul>
+        <button>Бесплатная консультация</button>
+      </div>
     </section>
   );
 };
