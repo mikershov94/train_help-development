fdc45616dc3956403667a3b7c3156d12b07fd78d
Michael E. Ershov <em@newton.finance>
2023-03-27 09:26:54 +0800

    Updated offer

--- src/views/Offer/Offer.tsx
+++ src/views/Offer/Offer.tsx
@@ -6,12 +6,20 @@ const Offer: FC = () => {
   return (
     <section className="offer">
       <div className="container">
-        <h1 className="offer__header">Легализация самостроя</h1>
+        <h1 className="offer__header">ЛЕГАЛИЗАЦИЯ САМОСТРОЯ</h1>
         <ul>
-          <li>Легализация через ГЗК</li>
-          <li>Узаконивание невыявленных объектов</li>
-          <li>Узаконивание через суд</li>
+          <li className="offer__label-pointer-1">ЛЕГАЛИЗАЦИЯ ЧЕРЕЗ ГЗК</li>
+          <li className="offer__label-pointer-2">
+            УЗАКОНИВАНИЕ НЕВЫЯВЛЕННЫХ ОБЪЕКТОВ
+          </li>
+          <li className="offer__label-pointer-3">УЗАКОНИВАНИЕ ЧЕРЕЗ СУД</li>
         </ul>
+        <div className="offer__pointer-horizontal-1" />
+        <div className="offer__pointer-rotated-1" />
+        <div className="offer__pointer-horizontal-2" />
+        <div className="offer__pointer-rotated-2" />
+        <div className="offer__pointer-horizontal-3" />
+        <div className="offer__pointer-rotated-3" />
         <ul>
           <li>от 150 тыс. рублей</li>
           <li>от 60 дней</li>
